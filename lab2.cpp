8c3c56a9e5de1f23f4ccdf9fd428dbf3c4ab76e7
Adam Mustaqim <adammustaqimrosli@gmail.com>
2025-07-01 07:56:09 +0800

    Create lab2.cpp

---
+++ lab2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    const int N = 3;
+    int arr[N][N] = {
+        {1, 0, 3},
+        {2, 5, 6},
+        {9, 4, 8}
+    };
+
+    int* ptr = (int*)arr;
+    sort(ptr, ptr + N * N);
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
+            cout << arr[i][j] << " ";
+        cout << "\n";
+    }
+
+    return 0;
+}
